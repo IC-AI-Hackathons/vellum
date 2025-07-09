4f212fc3fb37317fc8d653ab468401f9c48bf028
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2025-07-09 12:29:19 +0100

    update docs

--- src/vellum/utils/images.py
+++ src/vellum/utils/images.py
@@ -6,6 +6,6 @@ __all__ = ['extract_images']
 
 def extract_images(file_name, dpi=500):
     """
-    Extract image data from a PDF file.
+    Extract a list of images from a PDF file.
     """
     return pdf2image.convert_from_path(file_name, dpi)
