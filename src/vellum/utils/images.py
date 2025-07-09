7286ad713ddd54a57e40375ad06a8932da191879
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2025-07-09 12:28:03 +0100

    add extract_images

--- src/vellum/utils/images.py
+++ src/vellum/utils/images.py
@@ -1,13 +1,11 @@
 import pdf2image
 
-__all__ = ["extract_images"]
+
+__all__ = ['extract_images']
 
 
 def extract_images(file_name, dpi=500):
     """
     Extract image data from a PDF file.
     """
-    pages = pdf2image.convert_from_path(file_name, dpi)
-
-    return pages
-
+    return pdf2image.convert_from_path(file_name, dpi)
