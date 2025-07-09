0550177442c70031942cf698447679ccfcf459bd
enwask <enwask@ucf.edu>
2025-07-09 14:13:53 +0100

    Fixed image splitting

--- src/vellum/documents/images.py
+++ src/vellum/documents/images.py
@@ -1,5 +1,6 @@
-import pdf2image
+from pathlib import Path
 from PIL.Image import Image
+import pdf2image
 
 
 __all__ = ['extract_images', 'split_document_pages']
@@ -18,8 +19,13 @@ def split_document_pages(file_name: str, dpi: int = 500) -> list[str]:
     """
     images = extract_images(file_name, dpi)
 
+    # Make the page directory
     path, base_name = file_name.rsplit('/', 1)
-    file_names = [f'{path}/pages/{base_name}_{i}.png' for i in range(len(images))]
+    pages_directory = Path(path) / base_name.replace('.pdf', '_pages')
+    pages_directory.mkdir(parents=True, exist_ok=True)
+
+    # Save the images to the pages directory
+    file_names = [f"{pages_directory / ('page_' + str(i))}.png" for i in range(len(images))]
     for i, image in enumerate(images):
         image.save(file_names[i], 'PNG')
 
