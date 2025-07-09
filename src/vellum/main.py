31068a3f9da2f964a6d3845c4c70dfdf58d1b755
enwask <enwask@ucf.edu>
2025-07-09 13:22:07 +0100

    Embed in batches

--- src/vellum/main.py
+++ src/vellum/main.py
@@ -2,18 +2,24 @@ import torch
 from vellum.llm.models import embeddings_model, embeddings_processor
 from PIL import Image
 
+from vellum.documents.images import extract_images
+
 if __name__ == "__main__":
-    images = [
-        Image.new('RGB', (100, 100), color='red'),
-        Image.new('RGB', (100, 100), color='green'),
-    ]
+    # Load images from devito.pdf
+    print("Extracting images...")
+    images = extract_images('assets/devito.pdf')
+    print(f"Loaded {len(images)} images")
 
-    print("processing images...")
-    batch_images = embeddings_processor.process_images(images) \
-        .to(embeddings_model.device)
+    batch_size = 8
+    for i in range(0, len(images), batch_size):
+        batch = images[i:min(i + batch_size, len(images))]
 
-    print("getting embeddings...")
-    with torch.no_grad():
-        image_embeddings = embeddings_model(**batch_images)
+        print("Processing images...")
+        batch_images = embeddings_processor.process_images(batch) \
+            .to(embeddings_model.device)
 
-    print(image_embeddings)
+        print("Getting embeddings...")
+        with torch.no_grad():
+            image_embeddings = embeddings_model(**batch_images)
+
+        print(image_embeddings)
