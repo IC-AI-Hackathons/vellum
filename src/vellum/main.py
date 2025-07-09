af1526d6c2bfbc1f64a790309acceeed9a58f8b2
enwask <enwask@ucf.edu>
2025-07-09 14:15:22 +0100

    Fix things

--- src/vellum/main.py
+++ src/vellum/main.py
@@ -1,25 +1,11 @@
-import torch
-from vellum.llm.models import embeddings_model, embeddings_processor
+from vellum.documents.store import Documents
 from PIL import Image
 
-from vellum.documents.images import extract_images
+from vellum.documents.images import extract_images, split_document_pages
 
 if __name__ == "__main__":
     # Load images from devito.pdf
-    print("Extracting images...")
-    images = extract_images('assets/devito.pdf')
-    print(f"Loaded {len(images)} images")
-
-    batch_size = 8
-    for i in range(0, len(images), batch_size):
-        batch = images[i:min(i + batch_size, len(images))]
-
-        print("Processing images...")
-        batch_images = embeddings_processor.process_images(batch) \
-            .to(embeddings_model.device)
-
-        print("Getting embeddings...")
-        with torch.no_grad():
-            image_embeddings = embeddings_model(**batch_images)
-
-        print(image_embeddings)
+    image_uris = split_document_pages('assets/devito.pdf')
+    print("images:", image_uris)
+    # images = extract_images('assets/devito.pdf')
+    # for
